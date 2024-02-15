b8f650e6901667fd9ce64accb541fbe9c5d36cf5
taurgus <100350635+taurgus@users.noreply.github.com>
2024-02-15 09:58:31 +0200

    http server routes

---
+++ http_server_routes.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const port = 3001;
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+
+    const reqUrl = url.parse(req.url).pathname
+    
+if (reqUrl == "/") {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.writeHead(200);
+    res.write("Olet hakemistössä /");
+    res.end();
+}
+else if (reqUrl == "/hello") {
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8') // charset utf 8 = suomi
+    res.writeHead(200);
+    res.write("Olet hakemistossa /hello");
+    res.end();
+
+} else {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.writeHead(404);
+    res.write("Ei löydy 404 Not Found");
+    res.end();
+}
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
